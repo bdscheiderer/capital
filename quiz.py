2993f80180b550041798ba624fc3dc20535482ec
bdscheiderer <bdscheiderer@gmail.com>
2017-12-26 20:29:32 -0600

    update styles and finish results

--- quiz.py
+++ quiz.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, session, redirect, url_for, escape, request
 from capital_data import Capitals
 import random
 
-
 app = Flask(__name__)
+app.secret_key = "anyrandomstring"
 
 Capitals = Capitals()
 
@@ -14,33 +14,12 @@ def index():
 @app.route("/quiz", methods = ["POST"])
 def quiz():
     if request.method == "POST":
-        if request.form['Submit'] == "20 Questions":
+        if request.form['Submit'] == "20":
             n = 20
         else:
             n = 50
-    total_correct = 0
-    allstates = list(Capitals.keys())
-    cities = []
-    capitals_states = []
-    if n < 50:
-        states = random.sample(allstates, n)
-    else:
-        states = allstates
-    for state in states:
-        allcities = list(Capitals.values())
-        # identify correct answer
-        capital = Capitals[state]
-        # get three more possible answers and random shuffle answers
-        allcities.remove(capital)
-        cities = random.sample(allcities, 3)
-        cities.append(capital)
-        random.shuffle(cities)
-        # add state to list of cities at fifth index
-        cities.append(state)
-        # create list of cities and states to pass to quiz form
-        capitals_states.append(cities)
-        # shuffle the quesitons
-        random.shuffle(capitals_states)
+    session['number'] = n
+    capitals_states = questions(n, Capitals)
     return render_template("quiz.html", capitals_states = capitals_states)
 
 @app.route('/about')
@@ -55,16 +34,86 @@ def map():
 def result():
     if request.method == "POST":
         answers = request.form
-        correct = check(answers)
-        return render_template('result.html', n = correct)
+        length = len(answers)
+        number = session['number']
+        total_correct, wrong = check(answers, Capitals)
+        if length == 0:
+            return render_template('result_error.html')
+        elif total_correct == number:
+            return render_template('result_perfect.html', total_correct = total_correct, number = number)
+        elif total_correct / number > .699:
+            return render_template('result.html', total_correct = total_correct, message = "Greet score!", wrong = wrong)
+        else:
+            return render_template('result_warning.html', total_correct = total_correct, message = "Keep practicing", wrong = wrong)
+
+
+'''
+    if request.method == "POST":
+        answers = request.form
+        total_correct, message = check(answers, Capitals)
+        return render_template('result.html', total_correct = total_correct, message = message)
+'''
 
 @app.route('/stats')
 def stats():
     return render_template('stats.html')
 
-def check(answers):
-    for answer in answers: print(answers[answer], answer)
-    return 99
+def questions(n, Capitals):
+        allstates = list(Capitals.keys())
+        cities = []
+        capitals_states = []
+        if n < 50:
+            states = random.sample(allstates, n)
+        else:
+            states = allstates
+        for state in states:
+            # identify correct answer
+            capital = Capitals[state]
+            # get three more possible answers to create list of four
+            allcities = list(Capitals.values())
+            allcities.remove(capital)
+            cities = random.sample(allcities, 3)
+            cities.append(capital)
+            # shuffle the four possible answers
+            random.shuffle(cities)
+            # add state to list of cities at fifth index
+            cities.append(state)
+            # create list of the list of four cities and state to pass to quiz
+            capitals_states.append(cities)
+        # shuffle the list of quesitons
+        random.shuffle(capitals_states)
+        return capitals_states
+
+def check(answers, Capitals):
+    total_correct = 0
+    wrong = []
+    # answers is a dict, keys are the states, and values are the capital guess
+    for answer in answers:
+        state = answer
+        city_quess = answers[answer]
+        city_correct = Capitals[state]
+        if city_correct == city_quess:
+            total_correct = total_correct + 1
+        else:
+            wrong.append([city_correct, state])
+    print(total_correct, wrong)
+    return total_correct, wrong
+
+'''
+    n = session['number']
+    if len(answers) == 0:
+        message = "It appears no answers were submitted, please try again."
+    elif total_correct == n:
+        message = "Congratulations, perfect score!"
+    elif total_correct / n > .799:
+        message = "Great score!"
+    elif total_correct / n > .499:
+        message = "Keep practicing!"
+    else:
+         message = "Thank you for trying the quiz!"
+'''
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
