9c4db62cf7ebc77cd6f008b1a3b4c3cb1c1b3f0f
bdscheid <bdscheiderer@gmail.com>
2017-12-30 05:54:11 +0000

    stats working

--- sqltest.py
+++ sqltest.py
@@ -2,8 +2,10 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import numpy as np
 
 app = Flask(__name__)
+
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="bdscheid",
     password="password_db",
@@ -16,11 +18,38 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-print('Done')
+class scores(db.Model):
+
+    __tablename__ = "Scores"
+
+    ID = db.Column(db.Integer, primary_key=True)
+    Date = db.Column(db.String)
+    Quiz = db.Column(db.Integer)
+    Score = db.Column(db.Integer)
+
+def get_stats(n):
+    dat = scores.query.filter_by(Quiz=n).all()
+    data = []
+    for score in dat:
+        data.append(score.Score)
+    dict = {}
+    dict['mean'] = int(np.mean(data) + 0.5)
+    dict['median'] = int(np.median(data) + 0.5)
+    dict['std'] = int(np.std(data) + 0.5)
+    dict['attempts'] = len(data)
+    # create histograms
+#    dat20plot = get_plot(dat20, 21)
+#    dat50plot = get_plot(dat50, 51)
+    return dict
 
 @app.route("/")
 def hello():
     return "Welcome to Python Flask App!"
+
+stats20 = get_stats(20)
+stats50 = get_stats(50)
+print(stats20, stats50)
+print('Done')
 
 #if __name__ == "__main__":
 #    app.run()
